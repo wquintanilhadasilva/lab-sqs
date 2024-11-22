56021444d1f5a29d727ff45efa0230dd7b97f2e8
Wedson Quintanilha da Silva <wedson.silva@oobj.com.br>
2024-11-22 12:04:33 -0300

    updt

--- sqs-demo-consumer/src/main/java/com/lab/sqs/consumer/config/QueueManagement.java
+++ sqs-demo-consumer/src/main/java/com/lab/sqs/consumer/config/QueueManagement.java
@@ -3,9 +3,17 @@ package com.lab.sqs.consumer.config;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import software.amazon.awssdk.services.sqs.SqsAsyncClient;
-import software.amazon.awssdk.services.sqs.model.*;
+import software.amazon.awssdk.services.sqs.model.CreateQueueRequest;
+import software.amazon.awssdk.services.sqs.model.CreateQueueResponse;
+import software.amazon.awssdk.services.sqs.model.GetQueueAttributesRequest;
+import software.amazon.awssdk.services.sqs.model.GetQueueAttributesResponse;
+import software.amazon.awssdk.services.sqs.model.GetQueueUrlRequest;
+import software.amazon.awssdk.services.sqs.model.GetQueueUrlResponse;
+import software.amazon.awssdk.services.sqs.model.QueueAttributeName;
+import software.amazon.awssdk.services.sqs.model.QueueDoesNotExistException;
 
 import java.util.Map;
+import java.util.Optional;
 import java.util.concurrent.ExecutionException;
 @Slf4j
 @RequiredArgsConstructor
@@ -28,27 +36,13 @@ public class QueueManagement {
         }
     }
 
-    private String createQueue(String queueName) throws ExecutionException, InterruptedException {
+    public String createQueue(String queueName, Map<QueueAttributeName,String> attributes) throws ExecutionException, InterruptedException {
         log.info("Verificando se a fila [{}] existe...", queueName);
+        return checkExistQueue(queueName).orElseGet(() -> create(queueName, attributes));
+    }
 
-        try {
-            GetQueueUrlResponse response = sqsAsyncClient.getQueueUrl(GetQueueUrlRequest.builder().queueName(queueName).build()).get();
-            log.info("Fila [{}] já existe com URL [{}]", queueName, response.queueUrl());
-            return response.queueUrl();
-        } catch (ExecutionException e) {
-            if (e.getCause() instanceof QueueDoesNotExistException) {
-                log.info("Fila [{}] não encontrada. Criando uma nova.", queueName);
-
-                CreateQueueResponse createQueueResponse = sqsAsyncClient.createQueue(CreateQueueRequest.builder()
-                        .queueName(queueName)
-                        .build()).get();
-
-                log.info("Fila [{}] criada com sucesso. URL: [{}]", queueName, createQueueResponse.queueUrl());
-                return createQueueResponse.queueUrl();
-            } else {
-                throw e;
-            }
-        }
+    public String createQueue(String queueName) throws ExecutionException, InterruptedException {
+        return createQueue(queueName, null);
     }
 
     private String getQueueArn(String queueUrl) throws ExecutionException, InterruptedException {
@@ -61,17 +55,50 @@ public class QueueManagement {
     }
 
     private void createQueueWithRedrivePolicy(String queueName, String dlqArn) throws ExecutionException, InterruptedException {
-        log.info("Criando fila [{}] com DLQ associada [{}]", queueName, dlqArn);
+        checkExistQueue(queueName).ifPresentOrElse(uri -> log.warn("Fila [{}] já existe e não será criada!", queueName),
+                () -> {
+                    log.info("Criando fila [{}] com DLQ associada [{}]", queueName, dlqArn);
+                    String atr = String.format("{\"deadLetterTargetArn\":\"%s\",\"maxReceiveCount\":\"%s\"}", dlqArn, maxRetryCount);
+                    Map<QueueAttributeName, String> attributes = Map.of(QueueAttributeName.REDRIVE_POLICY,atr);
+                    try {
+                        String queueUrl = createQueue(queueName, attributes);
+                        log.info("Fila [{}] criada com sucesso e associada à DLQ [{}]. URL: [{}]",
+                                queueName, dlqArn, queueUrl);
+                    } catch (ExecutionException | InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+        );
+    }
 
-        String queueUrl = sqsAsyncClient.createQueue(CreateQueueRequest.builder()
-                .queueName(queueName)
-                .attributes(Map.of(
-                        QueueAttributeName.REDRIVE_POLICY, String.format(
-                                "{\"deadLetterTargetArn\":\"%s\",\"maxReceiveCount\":\"%s\"}", dlqArn, String.valueOf(maxRetryCount))
-                ))
-                .build()).get().queueUrl();
+    private String create(String queueName, Map<QueueAttributeName,String> attributes) {
+        log.info("Criando a fila [{}] com os atributos [{}].", queueName, attributes);
+        CreateQueueResponse createQueueResponse = null;
+        try {
+            createQueueResponse = sqsAsyncClient.createQueue(CreateQueueRequest.builder()
+                    .queueName(queueName)
+                    .attributes(attributes)
+                    .build()).get();
+        } catch (InterruptedException | ExecutionException e) {
+            throw new RuntimeException(e);
+        }
+        log.info("Fila [{}] criada com sucesso. URL: [{}]", queueName, createQueueResponse.queueUrl());
+        return createQueueResponse.queueUrl();
+    }
 
-        log.info("Fila [{}] criada com sucesso e associada à DLQ [{}]. URL: [{}]", queueName, dlqArn, queueUrl);
+    private Optional<String> checkExistQueue(String queueName) throws ExecutionException, InterruptedException {
+        try {
+            GetQueueUrlResponse response = sqsAsyncClient.getQueueUrl(GetQueueUrlRequest.builder().queueName(queueName).build()).get();
+            log.info("Fila [{}] já existe com URL [{}]", queueName, response.queueUrl());
+            return Optional.of(response.queueUrl());
+        } catch (ExecutionException | InterruptedException e) {
+            if (e.getCause() instanceof QueueDoesNotExistException) {
+                log.warn("Fila [{}] não encontrada.", queueName);
+                return Optional.empty();
+            } else {
+                throw e;
+            }
+        }
     }
 
 }
